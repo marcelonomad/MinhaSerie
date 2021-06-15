78724bb289421f1445354625cec71544e4bda030
Marcelo Negreiros <negreiros16@gmail.com>
2021-06-15 17:45:06 -0300

    Update Splash.kt

--- app/src/main/java/com/nomad/minhaserie/ui/Splash.kt
+++ app/src/main/java/com/nomad/minhaserie/ui/Splash.kt
@@ -1,12 +1,18 @@
 package com.nomad.minhaserie.ui
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.nomad.minhaserie.R
+import kotlinx.android.synthetic.main.activity_splash.*
+
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,18 @@ class Splash : AppCompatActivity() {
 
         val actionBar: ActionBar? = supportActionBar
         actionBar?.hide()
+
+        val scaleDown: ObjectAnimator = ObjectAnimator.ofPropertyValuesHolder(
+            imgSplash,
+            PropertyValuesHolder.ofFloat("scaleX", 1.1f),
+            PropertyValuesHolder.ofFloat("scaleY", 1.1f)
+        )
+        scaleDown.duration = 550
+
+        scaleDown.repeatCount = ObjectAnimator.INFINITE
+        scaleDown.repeatMode = ObjectAnimator.REVERSE
+
+        scaleDown.start()
 
         Handler(Looper.getMainLooper()).postDelayed({
             /* Create an Intent that will start the Menu-Activity. */
